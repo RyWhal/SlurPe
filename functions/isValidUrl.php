fea9ff588e10cdd72e0b9a20df0d5229b170e716
Ryan <ryanjpwhalen@gmail.com>
2014-04-04 20:15:46 -0700

    Update isValidUrl.php

--- functions/isValidUrl.php
+++ functions/isValidUrl.php
@@ -1,6 +1,7 @@
 <?php
 //function is just a fancy regex to look for url format
 //does not verify valid URL
+//It can't handle the end of a URL (.com, .org, etc)
 function isValidURL($url) {
 	return preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $url);
 }
