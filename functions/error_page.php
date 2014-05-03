dc524b86f1d87cc642debbb4f7ce0e7b72914c19
RyWhal <ryanjpwhalen@gmail.com>
2014-05-03 04:07:34 +0000

    added field for short URL

--- functions/error_page.php
+++ functions/error_page.php
@@ -4,18 +4,24 @@ function error_page($message, $search){
 	//this code is run if there was an error submitting the link. gives another chance to enter a URL	
 	if($search=='true'){
 		echo '
+			<html>
 			<div class="message" style="margin-left:17%;margin-right:25%">'.$message.'</div>
 			<div class="form_input">
 			<form name="input" action="url.php" class="urls" method="post">
 			<div class="col-lg-8">
 			<div class="input-group">
-			<input type="text" name="url_full" placeholder="Full URL: http://example.com" class="form-control">
-			<span class="input-group-btn"> <button class="btn btn-default" type="submit">Submit</button></span>
+			<input type="text" name="url_full" placeholder="Enter the URL you want to shorten" class="form-control">
+			<span class="input-group-btn"> <button class="btn btn-default" type="submit">SHORTEN</button></span>
 			</div><!-- /input-group -->
 			</div><!--col-lg-8-->
-			<div class="url_out" id="url_out" style="margin-left:17%"></div>
+			<div class="col-lg-8" sytle="width:50%;">
+			<h3 style="position:absolute;">http://slur.pe</h3>
+			<input type="text" id="url_short" name="url_short" placeholder="Custom-URL" maxlength="20" class="form-control" style="width:60%;margin-left:154px;margin-top:11px;">
+			</div><br><!-- /.col-lg-8 -->
 			</form>
-			</div><!-- form_input -->';
+			</div><!-- form_input -->
+			</html>
+			';
 	}
 	//success message. Shows message in jumbotron
 	else{
